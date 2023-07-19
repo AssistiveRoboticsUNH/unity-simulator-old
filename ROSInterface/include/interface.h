16e6ba739d098a511885b7102da38f082c2278ea
Paul Gesel <paulgesel@gmail.com>
2023-07-19 16:36:26 -0400

    support odom and actities

--- ROSInterface/include/interface.h
+++ ROSInterface/include/interface.h
@@ -20,7 +20,7 @@ struct Quaternion {
 
 };
 
-struct Vector3 {
+struct NativeVector3 {
     double x = 0;
     double y = 0;
     double z = 0;
@@ -29,14 +29,31 @@ struct Vector3 {
 
 struct NativeTransform {
     char *frame_id = nullptr;
-    Vector3 translation;
+    char *child_frame_id = nullptr;
+    NativeVector3 translation;
     Quaternion rotation;
+};
+
+struct NativeOdom {
+    NativeTransform pose;
+    double pose_covariance[36] = {};
+    NativeVector3 linear_velocity;
+    NativeVector3 angular_velocity;
+    double twist_covariance[36] = {};
+};
+
+struct NativeActivity {
+    int person_eating;
+    int person_taking_medicine;
 };
 
 void initROS();
 
 extern "C" {
 MY_LIB_API void PublishTF(std::intptr_t handle, NativeTransform *input);
+MY_LIB_API void PublishActivity(std::intptr_t handle, NativeActivity *input);
+MY_LIB_API void PublishOdom(std::intptr_t handle, NativeOdom *input);
+//MY_LIB_API void ReceiveCmdVel(std::intptr_t handle, NativeVector3 *output);
 MY_LIB_API std::intptr_t Init();
 MY_LIB_API void Destroy(std::intptr_t handle);
 }
--- ROSInterface/src/interface.cpp
+++ ROSInterface/src/interface.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "tf2_ros/transform_broadcaster.h"
 
@@ -16,16 +18,18 @@ using namespace std::chrono_literals;
 class MinimalPublisher : public rclcpp::Node {
 public:
     MinimalPublisher() : Node("minimal_publisher") {
-        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        eating_publisher_ = this->create_publisher<std_msgs::msg::Int32>("person_eating", 10);
+        taking_medicine_publisher_ = this->create_publisher<std_msgs::msg::Int32>("person_taking_medicine", 10);
+        odom_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
         tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
     }
 
-    void publish(const NativeTransform *input) {
+    void publishTF(const NativeTransform *input) {
         geometry_msgs::msg::TransformStamped t;
 
         t.header.stamp = this->get_clock()->now();
-        t.header.frame_id = "unity";
         t.child_frame_id = input->frame_id;
+        t.header.frame_id = input->child_frame_id;
         t.transform.translation.x = input->translation.x;
         t.transform.translation.y = input->translation.y;
         t.transform.translation.z = input->translation.z;
@@ -35,16 +39,50 @@ public:
         t.transform.rotation.w = input->rotation.w;
 
         tf_broadcaster_->sendTransform(t);
+    }
 
-        auto message = std_msgs::msg::String();
-        message.data = "hi!!";
-        publisher_->publish(message);
+    void PublishActivity(const NativeActivity *input) {
+
+        auto message = std_msgs::msg::Int32();
+        message.data = input->person_eating;
+        eating_publisher_->publish(message);
+        message.data = input->person_taking_medicine;
+        taking_medicine_publisher_->publish(message);
+    }
+
+    void PublishOdom(const NativeOdom *input) {
+        auto message = nav_msgs::msg::Odometry();
+        message.header.stamp = this->get_clock()->now();
+        message.header.frame_id = input->pose.frame_id;
+        message.child_frame_id = input->pose.child_frame_id;
+
+        message.pose.pose.position.x = input->pose.translation.x;
+        message.pose.pose.position.y = input->pose.translation.y;
+        message.pose.pose.position.z = input->pose.translation.z;
+        message.pose.pose.orientation.x = input->pose.rotation.x;
+        message.pose.pose.orientation.y = input->pose.rotation.y;
+        message.pose.pose.orientation.z = input->pose.rotation.z;
+        message.pose.pose.orientation.w = input->pose.rotation.w;
+        std::memcpy(&message.pose.covariance, input->pose_covariance, sizeof(input->pose_covariance));
+
+        message.twist.twist.linear.x = input->linear_velocity.x;
+        message.twist.twist.linear.y = input->linear_velocity.y;
+        message.twist.twist.linear.z = input->linear_velocity.z;
+
+        message.twist.twist.angular.x = input->angular_velocity.x;
+        message.twist.twist.angular.y = input->angular_velocity.y;
+        message.twist.twist.angular.z = input->angular_velocity.z;
+
+        std::memcpy(&message.twist.covariance, input->twist_covariance, sizeof(input->twist_covariance));
+        odom_publisher_->publish(message);
     }
 
 private:
 
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr eating_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr taking_medicine_publisher_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher_;
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 };
 
@@ -61,14 +99,23 @@ void initROS() {
 class ROSInterface {
 public:
 
-    ROSInterface(){
+    ROSInterface() {
         initROS();
         node_ = std::make_shared<MinimalPublisher>();
     }
 
     void PublishTF(NativeTransform *input) {
-        node_->publish(input);
+        node_->publishTF(input);
     }
+
+    void PublishActivity(NativeActivity *input) {
+        node_->PublishActivity(input);
+    }
+
+    void PublishOdom(NativeOdom *input) {
+        node_->PublishOdom(input);
+    }
+
 
     std::shared_ptr<MinimalPublisher> node_;
 
@@ -88,3 +135,17 @@ void PublishTF(std::intptr_t handle, NativeTransform *input) {
     ptr->PublishTF(input);
 }
 
+void PublishActivity(std::intptr_t handle, NativeActivity *input) {
+    auto ptr = (ROSInterface *) handle;
+    ptr->PublishActivity(input);
+}
+
+void PublishOdom(std::intptr_t handle, NativeOdom *input) {
+    auto ptr = (ROSInterface *) handle;
+    ptr->PublishOdom(input);
+}
+
+//void ReceiveCmdVel(std::intptr_t handle, NativeVector3 *output) {
+//    auto ptr = (ROSInterface *) handle;
+////    ptr->ReceiveOdom(output);
+//}
